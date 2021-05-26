8bdbf0c0caeb6c72787e0ae8ad87d382af61488b
OlenaLavro <lavrolavro2001lav@gmail.com>
2021-05-26 17:31:32 +0300

    commit stopwatch

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import StopwatchDisplay from "./StopwatchDisplay";
+
+function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [lastClickedTimeOnWaitBtn, setLastClickedTimeOnWaitBtn] = useState(0);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+  };
+
+  const handleWait = () => {
+    const timeNow = new Date().getTime();
+    if (timeNow - lastClickedTimeOnWaitBtn < 300) {
+      setIsRunning(false);
+    }
+
+    setLastClickedTimeOnWaitBtn(timeNow);
+  };
+
+  useEffect(() => {
+    const unsubscribe = new Subject();
+    interval(10)
+      .pipe(takeUntil(unsubscribe))
+      .subscribe(() => {
+        if (isRunning) {
+          setTime((value) => value + 1);
+        }
+      });
+    return () => {
+      unsubscribe.next();
+      unsubscribe.complete();
+    };
+  }, [isRunning]);
+
+  return (
+    <div className="stopwatch-container">
+      <header className="header">Stopwatch</header>
+
+      <main className="main">
+        <StopwatchDisplay time={time} />
+
+        <div className="button-list">
+          {!isRunning && (
+            <button className="button" onClick={() => handleStart()}>
+              START
+            </button>
+          )}
+          {isRunning && (
+            <button className="button" onClick={() => handleStop()}>
+              STOP
+            </button>
+          )}
+          <button className="button" onClick={() => handleWait()}>
+            WAIT
+          </button>
+          <button className="button" onClick={() => handleReset()}>
+            RESET
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Stopwatch;
---
+++ src/components/StopwatchDisplay.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../App.css";
+
+function StopwatchDisplay(props) {
+  return (
+    <div className="stopwatch-wrapper">
+      <div className="stopwatch-display">
+        <div className="hours">
+          {("0" + Math.floor((props.time / (1000 * 60 * 60)) % 24)).slice(-2)}
+        </div>
+        <div className="delimiter">:</div>
+        <div className="minutes">
+          {("0" + Math.floor(props.time / 6000)).slice(-2)}
+        </div>
+        <div className="delimiter">:</div>
+        <div className="seconds">
+          {("0" + Math.floor((props.time / 100) % 60)).slice(-2)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StopwatchDisplay;

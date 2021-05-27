d70a1d7920eafea7c47eef67deff88d0a9a473bb
OlenaLavro <lavrolavro2001lav@gmail.com>
2021-05-27 18:42:37 +0300

    commit changes2

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,31 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import { interval, Subject, fromEvent, asyncScheduler } from "rxjs";
-import { takeUntil, throttleTime, withLatestFrom } from "rxjs/operators";
+import { interval, Subject, fromEvent } from "rxjs";
+import { takeUntil, buffer, filter, debounceTime, map } from "rxjs/operators";
 import StopwatchDisplay from "./StopwatchDisplay";
 
 function Stopwatch() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const [lastClickedTimeOnWaitBtn, setLastClickedTimeOnWaitBtn] = useState(0);
-
-  const handleStop = () => {
-    setIsRunning(false);
-    setTime(0);
-  };
-
-  const handleReset = () => {
-    setTime(0);
-  };
-
-  const handleWait = () => {
-    const timeNow = new Date().getTime();
-    if (timeNow - lastClickedTimeOnWaitBtn < 300) {
-      setIsRunning(false);
-    }
-
-    setLastClickedTimeOnWaitBtn(timeNow);
-  };
 
   useEffect(() => {
     const unsubscribe = new Subject();
@@ -43,21 +24,44 @@ function Stopwatch() {
   }, [isRunning]);
 
   useEffect(() => {
-    const startClick = fromEvent(
+    const startClick$ = fromEvent(
       document.getElementById("start-btn"),
       "click"
     ).subscribe(() => setIsRunning(true));
 
-    const resetClick = fromEvent(
+    const resetClick$ = fromEvent(
       document.getElementById("reset-btn"),
       "click"
     ).subscribe(() => {
       setTime(0);
     });
 
+    const stopClick$ = fromEvent(
+      document.getElementById("stop-btn"),
+      "click"
+    ).subscribe(() => {
+      setIsRunning(false);
+      setTime(0);
+    });
+
+    const waitEvent$ = fromEvent(document.getElementById("wait-btn"), "click");
+    const buffWaitClick$ = waitEvent$.pipe(debounceTime(300));
+    const waitClick$ = waitEvent$.pipe(
+      buffer(buffWaitClick$),
+      map((list) => {
+        return list.length;
+      }),
+      filter((x) => x === 2)
+    );
+    waitClick$.subscribe(() => {
+      setIsRunning(false);
+    });
+
     return () => {
-      startClick.unsubscribe();
-      resetClick.unsubscribe();
+      startClick$.unsubscribe();
+      stopClick$.unsubscribe();
+      resetClick$.unsubscribe();
+      waitClick$.unsubscribe();
     };
   }, []);
 
@@ -69,21 +73,17 @@ function Stopwatch() {
         <StopwatchDisplay time={time} />
 
         <div className="button-list">
-          {!isRunning && (
+          {true && (
             <button className="button" id="start-btn">
               START
             </button>
           )}
-          {isRunning && (
-            <button
-              className="button"
-              id="stop-btn"
-              onClick={() => handleStop()}
-            >
+          {true && (
+            <button className="button" id="stop-btn">
               STOP
             </button>
           )}
-          <button className="button" onClick={() => handleWait()}>
+          <button className="button" id="wait-btn">
             WAIT
           </button>
           <button className="button" id="reset-btn">
